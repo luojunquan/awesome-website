18347fd3f98f82ce4e0db965ea1517b0c8664a09
luojunquan <ljq906416@gmail.com>
2019-01-15 17:37:41 +0800

    提交orm

--- www/app.py
+++ www/app.py
@@ -5,8 +5,7 @@
 # @File    : app.py
 # @Software: PyCharm
 import logging;logging.basicConfig(level=logging.INFO)
-import asyncio,os,json,time
-from datetime import datetime
+import asyncio
 from aiohttp import web
 
 routes = web.RouteTableDef()
@@ -15,21 +14,14 @@ routes = web.RouteTableDef()
 @routes.get('/')
 async def index(request):
     await asyncio.sleep(0.5)
-    text = '这是首页'
+    text = b'<h1>Awesome Website</h1>'
     return web.Response(body=text.encode('gbk'),content_type='text/html')
 
-@routes.get('/about')
-async def about(request):
-    await asyncio.sleep(0.5)
-    return web.json_response({'name':'about','sex':'boy'},content_type='text/html')
-
-#建立服务器应用，持续监听本地9000端口的http请求，并异步对首页"/"进行响应
+#建立服务器应用，并异步对首页"/"进行响应
 
 def init():
     app = web.Application()
     app.add_routes(routes)
-    logging.info('server started at http://127.0.0.1:8080...')
     web.run_app(app)
-
 
 init()
